a884343e99b01639825f16e309fea4bec60a44f2
durul <adurul17@hotmail.com>
2017-04-25 09:29:30 -0400

    part 1
    part 1

---
+++ combat/combat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  combat
+//
+//  Created by durul dalkanat on 4/24/17.
+//  Copyright © 2017 durul dalkanat. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
